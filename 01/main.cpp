0ffc0e04da2362ae90147b204c97dc48ccc552e2
Peter Lilley <plilley2005@hotmail.com>
2022-12-01 06:19:45 +0000

    Day 1

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,14 +1,36 @@
 #include <common.hpp>
 
+#define PART 2
+
 int main(int argc, char** argv) {
   std::string str = get_file(argc, argv);
   std::string::const_iterator curr_it = str.begin();
 
-  double answer = 0;
+  std::vector<int> cals;
 
+  int curr = 0;
+  while (curr_it != str.end()) {
+    if (*curr_it == '\n') {
+      cals.push_back(curr);
+      curr = 0;
+      ++curr_it;
+    }
 
+    curr += parse_double(curr_it, str.end());
+    ++curr_it;
+  }
 
-  fmt::print("{}\n", answer);
+  std::sort(cals.begin(), cals.end());
 
+  int total = 0;
+
+#if PART == 1
+  total = cals.back();
+#elif PART == 2
+  total = std::accumulate(cals.end() - 3, cals.end(), 0);
+#endif
+
+  fmt::print("{}\n", total);
+  
   return 0;
 }
